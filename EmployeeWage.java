9938c924b74a913e11a228eacb82e201671e7cc5
Apoorva1217 <Apoorva1217@gmail.com>
2020-07-17 13:10:58 +0530

    Added:calculating wages till number of working days ortotal working hours per month is reached

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,30 +4,34 @@ public class EmployeeWage
 	public static final int FULL_TIME=2;
 	public static final int EMP_RATE_PER_HR=20;
 	public static final int NUM_OF_WORKING_DAYS=20;
+        public static final int MAX_HRS_IN_MONTH=100;
 
 	public static void main(String[] args)
 	{
 		//variables
 		int empHrs=0;
-		int empWage=0;
-		int totalEmpWage=0;
+		int totalEmpHrs=0;
+		int totalWorkingDays=0;
 		//computation
-		for(int day=0;day<NUM_OF_WORKING_DAYS;day++)
+		while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
 		{
+			totalWorkingDays++;
 			int empCheck=(int) Math.floor(Math.random()*10)%3;
 			switch(empCheck)
 			{
 				case PART_TIME:
 					empHrs=4;
+					break;
 				case(FULL_TIME):
 					empHrs=8;
+					break;
 				default:
 					empHrs=0;
 			}
-			empWage=empHrs*EMP_RATE_PER_HR;
-			totalEmpWage+=empWage;
-			System.out.println("Employee Wage:"+empWage);
+			totalEmpHrs+=empHrs;
+			System.out.println("Day:"+totalWorkingDays+" EmployeeHours:"+empHrs);
 		}
+		int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HR;
 		System.out.println("Total Employee Wage:"+totalEmpWage);
 	}
 }
