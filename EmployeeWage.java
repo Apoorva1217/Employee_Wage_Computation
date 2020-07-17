b170c95cb67cc7718446ddf2724751d71a931d7f
Apoorva1217 <Apoorva1217@gmail.com>
2020-07-17 12:46:32 +0530

    Added:part time and full time daily employee wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,21 +2,18 @@ public class EmployeeWage
 {
 	public static void main(String[] args) 
 	{
-		int FULL_TIME=1;
+		int PART_TIME=1;
+		int FULL_TIME=2;
 		int EMP_WAGE_PER_HR=20;
 		int empHrs=0;
 		int empWage=0;
-		double empCheck=Math.floor(Math.random()*10)%2;
+		double empCheck=Math.floor(Math.random()*10)%3;
 		if(empCheck==FULL_TIME)
-		{
-			System.out.println("Employee is Present.");
 			empHrs=8;
-		}
+		else if(empCheck==PART_TIME)
+			empHrs=4;
 		else
-		{
-			System.out.println("Employee is Absent.");
 			empHrs=0;
-		}
 		empWage=empHrs*EMP_RATE_PER_HR;
 		System.out.println("Daily Employee Wage:"+empWage):
 	}
